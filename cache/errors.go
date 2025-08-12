35c127358d26d0fb5cdfefeb6b71d6d5d5b516a7
Samuel Reynolds <samuelreynolds@mac.lan>
2025-08-12 18:43:52 -0400

    updated is cache miss function

--- cache/errors.go
+++ cache/errors.go
@@ -3,9 +3,10 @@ package cache
 import (
 	"errors"
 
+	"github.com/eko/gocache/lib/v4/store"
 	"github.com/redis/go-redis/v9"
 )
 
 func IsCacheMissErr(err error) bool {
-	return errors.Is(err, redis.Nil)
+	return errors.Is(err, redis.Nil) || errors.Is(err, store.NotFound{})
 }
--- store/store_test.go
+++ store/store_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ooqls/go-cache/cache"
 	"github.com/ooqls/go-db/redis"
 	"github.com/ooqls/go-db/testutils"
 	"github.com/stretchr/testify/assert"
@@ -42,5 +43,39 @@ func TestRedisStore(t *testing.T) {
 	err = store.Get(context.Background(), "key", &updatedObj)
 	assert.Nil(t, err)
 	assert.Equal(t, "updated value", updatedObj.V)
+}
 
+func TestMemStore(t *testing.T) {
+	store := NewMemStore(10 * time.Second)
+
+	obj := Obj{V: "value"}
+	err := store.Set(context.Background(), "key", obj)
+	assert.Nil(t, err)
+
+	var updatedObj Obj
+	err = store.Get(context.Background(), "key", &updatedObj)
+	assert.Nil(t, err)
+	assert.Equal(t, obj.V, updatedObj.V)
+
+	err = store.Update(context.Background(), "key", func(fn func(target any) error) (any, error) {
+		var obj Obj
+
+		err := fn(&obj)
+		assert.Nil(t, err)
+
+		obj.V = "updated value"
+		return obj, nil
+	})
+
+	err = store.Get(context.Background(), "key", &updatedObj)
+	assert.Nil(t, err)
+	assert.Equal(t, "updated value", updatedObj.V)
+}
+
+func TestMemStore_Get_NotFound(t *testing.T) {
+	store := NewMemStore(10 * time.Second)
+
+	var obj Obj
+	err := store.Get(context.Background(), "key", &obj)
+	assert.True(t, cache.IsCacheMissErr(err))
 }
